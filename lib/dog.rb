1dc3934ae9edc0aad8e405f8f4697212f353569b
Nicole Stellatos <stellatosnicole@gmail.com>
2020-07-21 00:14:42 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,16 +1,9 @@
 class Dog 
-#  def name= (dogs_name)
-#    @name = dogs_name
-#  end
-#def name
-#  @name
-#end
-  
-    def name=(dogs_name)
+ def name=(dogs_name)
     @name = dogs_name
   end
-  
-  def name
-    @name 
-  end
+def name
+  @name
 end
+end  
+    
